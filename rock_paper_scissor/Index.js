65d83afa8bf5d5068f597d01210753e573fd94fc
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-09-13 18:21:22 +0530

    updated scores function and fix bug of game function

--- rock_paper_scissor/Index.js
+++ rock_paper_scissor/Index.js
@@ -1,23 +1,14 @@
 let player;
-let count=0;
+let P_count = 0;
+let C_count = 0;
 
-const score = document.querySelector("#score");
-score.textContent=count;
+const playerscore = document.querySelector("#pscore");
+const computerscore = document.querySelector("#cscore");
 
 const playerSelection = document.querySelector("#player");
 
-//random weapon for computer
-let computer;
-let selection = Math.random();
-if (selection < 0.34) {
-  computer = "rock";
-} else if (selection <= 0.67) {
-  computer = "paper";
-} else computer = "scissor";
-
-
 //game
- function Game(x,y) {
+function Game(x, y) {
   if (
     (x == "rock" && y == "rock") ||
     (x == "paper" && y == "paper") ||
@@ -30,23 +21,35 @@ if (selection < 0.34) {
     (x == "scissor" && y == "rock")
   ) {
     playerSelection.textContent = "Loose!!";
+    C_count += 1;
+    computerscore.textContent = C_count;
   } else if (
     (x == "rock" && y == "scissor") ||
     (x == "paper" && y == "rock") ||
     (x == "scissor" && y == "paper")
   ) {
     playerSelection.textContent = "Winn!!";
+    P_count += 1;
+    playerscore.textContent = P_count;
   } else playerSelection.textContent = "Enter Correct Option!!";
-};
+}
 
 // player weapon selection
 const buttons = document.querySelectorAll("button");
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+
+    //random weapon for computer
+    let computer;
+    let selection = Math.random();
+    if (selection < 0.34) {
+      computer = "rock";
+    } else if (selection <= 0.67) {
+      computer = "paper";
+    } else computer = "scissor";
+
     player = button.id;
-    //check
-    console.log(player,computer);
-    Game(player,computer);
+    Game(player, computer);
   });
 });
-
