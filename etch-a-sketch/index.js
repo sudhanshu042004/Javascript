4cf9aae03b5ceb62a0a72f16105c609fb666fff4
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-09-15 00:01:25 +0530

    add function of clear display and then show div

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,21 +1,37 @@
 const container = document.querySelector("#container");
 //take input from user and set maxlength
 const submit = document.querySelector("#submit");
+let resetCount=0;
+
 
 submit.addEventListener("click", () => {
+  resetCount=++resetCount;
   var num = document.querySelector("#num").value;
-  if (num <= 64) {
-    for (i = 1; i <= num * num; i++) {
-      let height = 500/num;
-      height = height+"px";
-      let width = 500/num;
-      width = width+"px";
-      console.log(height,width);
-      const inDiv = document.createElement("div");
-      inDiv.classList.add("draw");
-      inDiv.style.height=height;
-      inDiv.style.width=width;
-      container.appendChild(inDiv);
+  if(resetCount==2){
+    container.innerHTML="";
+  }
+  setTimeout(() => {
+    if (num <= 64) {
+      for (i = 1; i <= num * num; i++) {
+        let height = 500 / num;
+        height = height + "px";
+        let width = 500 / num;
+        width = width + "px";
+        
+        const inDiv = document.createElement("div");
+        inDiv.classList.add("draw");
+        inDiv.style.height = height;
+        inDiv.style.width = width;
+        container.appendChild(inDiv);
+        inDiv.addEventListener("mouseover",()=>{
+          inDiv.style.backgroundColor="blue";
+        })
+      }
+    } else {
+      alert("Max length is 64");
+      window.location.reload();
     }
-  } else alert("Max length is 64");
+  }, 500);
+  
+  
 });
