edcc0fdbb96930ea66a634ff208fae9f01ca126a
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-09-14 23:12:09 +0530

    added grid generator funtion

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,9 +1,21 @@
 const container = document.querySelector("#container");
-//take input from user and fic maxlength
+//take input from user and set maxlength
 const submit = document.querySelector("#submit");
+
 submit.addEventListener("click", () => {
-  const num = document.querySelector("#num").value;
+  var num = document.querySelector("#num").value;
   if (num <= 64) {
-    console.log(num);
+    for (i = 1; i <= num * num; i++) {
+      let height = 500/num;
+      height = height+"px";
+      let width = 500/num;
+      width = width+"px";
+      console.log(height,width);
+      const inDiv = document.createElement("div");
+      inDiv.classList.add("draw");
+      inDiv.style.height=height;
+      inDiv.style.width=width;
+      container.appendChild(inDiv);
+    }
   } else alert("Max length is 64");
 });
