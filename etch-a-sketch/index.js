4ce7f7009d49320dee04521c93d526655c34bb9d
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-09-15 22:31:50 +0530

    enchance UI

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,37 +1,32 @@
 const container = document.querySelector("#container");
 //take input from user and set maxlength
 const submit = document.querySelector("#submit");
-let resetCount=0;
-
+let resetCount = 0;
 
 submit.addEventListener("click", () => {
-  resetCount=++resetCount;
+  resetCount = ++resetCount;
   var num = document.querySelector("#num").value;
-  if(resetCount==2){
-    container.innerHTML="";
+  if (resetCount >= 2) {
+    container.innerHTML = "";
   }
-  setTimeout(() => {
-    if (num <= 64) {
-      for (i = 1; i <= num * num; i++) {
-        let height = 500 / num;
-        height = height + "px";
-        let width = 500 / num;
-        width = width + "px";
-        
-        const inDiv = document.createElement("div");
-        inDiv.classList.add("draw");
-        inDiv.style.height = height;
-        inDiv.style.width = width;
-        container.appendChild(inDiv);
-        inDiv.addEventListener("mouseover",()=>{
-          inDiv.style.backgroundColor="blue";
-        })
-      }
-    } else {
-      alert("Max length is 64");
-      window.location.reload();
+  if (num <= 64) {
+    for (i = 1; i <= num * num; i++) {
+      let height = 500 / num;
+      height = height + "px";
+      let width = 500 / num;
+      width = width + "px";
+
+      const inDiv = document.createElement("div");
+      inDiv.classList.add("draw");
+      inDiv.style.height = height;
+      inDiv.style.width = width;
+      container.appendChild(inDiv);
+      inDiv.addEventListener("mouseover", () => {
+        inDiv.style.backgroundColor = "blue";
+      });
     }
-  }, 500);
-  
-  
+  } else {
+    alert("Max length is 64");
+    window.location.reload();
+  }
 });
