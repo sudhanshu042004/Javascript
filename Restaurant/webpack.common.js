0676d0d48f127b4759ba074c7f17a958e1ae1689
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-10-19 18:19:40 +0530

    fix bug

--- Restaurant/webpack.common.js
+++ Restaurant/webpack.common.js
@@ -1,13 +1,18 @@
 const path = require('path');
 
+
 module.exports={
     entry: {
         app: './src/index.js',
     },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: 'Production',
+    //     }),
+    // ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean:true,
     },
     module: {
         rules: [
