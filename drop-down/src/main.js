b10109ce59fa5abbf73b10768dd497c8aa115a4e
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-11-17 11:28:50 +0530

    added drop-down

---
+++ drop-down/src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import { DropDown } from './DropDown';
+
+const button = document.querySelector(".button");
+const content = document.querySelector(".content");
+let option = ["option1","option2","option3","option4"];
+DropDown(button,content,option);
